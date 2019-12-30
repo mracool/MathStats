b3b4e24e2e1cdd2462417e42f7172fdd3fd9bdfd
mracool <mmracool@gmail.com>
2019-12-30 07:47:53 +0300

    poped Lyisan's part)

--- task3part2.py
+++ task3part2.py
@@ -1,20 +1,20 @@
 import pandas as pd
 from scipy import stats
 
-def lower_confidence_limit(n, s, alpha):
-    func = (s*(n-1))/stats.chi2.ppf(alpha, df = n - 1)
+
+def lower_confidence_bound(n, s, alpha):
+    s = s * (n - 1) / (n)
+    # ask if the variance right, cause it's unbaised here
+    func = (s * n) / (stats.chi2.ppf(1 - alpha, df=n - 1))
     return func
 
 file_name = 'r3z2.csv'
 
 data = pd.read_csv(file_name)
 
-alpha = 0.99
+alpha = 1 - (0.99)
+print(alpha)
 s_range = int(data.size)
-s_var = int(data.var())
+s_var = float(data.var())
 print(s_range, s_var, alpha)
-# c = lower_confidence_limit(s_range, s_var, alpha)
-print(lower_confidence_limit(s_range, s_var, alpha))
-
-
-
+print(lower_confidence_bound(s_range, s_var, alpha))
